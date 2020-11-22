75412369448f91f1ad791c189f8c198e0d4523a1
Tomek <tomasz.dworniczak@hotmail.com>
2020-11-22 16:39:26 +0100

    Add .gitignore, fix slider

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 var slideIndex = 1;
+var timer;
+var timer2;
+
 showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
 
 function currentSlide(n) {
     showSlides(slideIndex = n);
@@ -12,11 +19,17 @@ function showSlides(n) {
     if (n > slides.length) {slideIndex = 1}
         if (n < 1) {slideIndex = slides.length}
         for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
+            slides[i].style.transition = "2.0s";
+            slides[i].style.opacity = "0";
         }
         for (i = 0; i < dots.length; i++) {
             dots[i].className = dots[i].className.replace(" active", "");
         }
-    slides[slideIndex-1].style.display = "block";
+    slides[slideIndex-1].style.transition  = "2.0s";
+    slides[slideIndex-1].style.opacity = "1";
     dots[slideIndex-1].className += " active";
+
+    clearTimeout(timer);
+    timer = setTimeout(() => plusSlides(1), 7000);
+
 }
